a0297220f540c0cdadc35cf758c998e5639c79e3
amir-hosein-003 <ah.kanani003@gmail.com>
2025-04-14 09:15:37 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar"
+import HeroSection from "./components/HeroSection"
+import Products from "./components/Products"
+
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <Products />
+    </>
+  )
+}
+
+export default App
